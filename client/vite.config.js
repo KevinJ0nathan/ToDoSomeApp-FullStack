915210884a8c0bf9704e9da8114d193592b663a9
Kevin Jonathan <realkevinjonathan@gmail.com>
2025-05-30 15:15:45 +0700

    deploy cicd pipeline
    Signed-off-by: Kevin Jonathan <realkevinjonathan@gmail.com>

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,12 +3,17 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
-export default defineConfig(() => {
+export default defineConfig(({ mode }) => {
   return {
     server: {
       proxy: {
         "/service": {
-          target: "http://localhost:5000",
+          target: mode === "development"
+            ? "http://localhost:5000"
+            : "https://e2425-wads-l4bcg3-server-kevin.csbihub.id",
+          changeOrigin: true,
+          secure: mode !== "development",
+          ws: true,
         },
       },
     },
